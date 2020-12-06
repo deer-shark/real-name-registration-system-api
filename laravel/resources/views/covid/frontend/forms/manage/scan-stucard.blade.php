cf9f781b8348facc02a505de1e9c902d43d2bc26
You Cheng Huang <53697217+yc97463@users.noreply.github.com>
2020-12-06 18:34:35 +0800

    更新掃QR管理端介面

--- laravel/resources/views/covid/frontend/forms/manage/scan-stucard.blade.php
+++ laravel/resources/views/covid/frontend/forms/manage/scan-stucard.blade.php
@@ -36,58 +36,70 @@
     });
     </script>
     --}}
-        <div class="container ct-s">
-            <div class="row">
-                <div class="col-sm-6">
-                    <form action="{{ route('post.form.stu-card') }}" method="POST" class="stu-in">
-                        <div class="row">
-                            <div class="col-sm">
-                                <div class="form-group">
-                                    <label for="time"><i class="far fa-clock"></i> 時間</label>
-                                    <input type="text" class="form-control disable" id="time" placeholder="系統自動填入，請勿更動" disabled>
+        <form action="{{ route('post.form.stu-card') }}" method="POST" class="stu-in">
+            <div class="container ct-s">
+                <div class="row">
+                    <div class="col-sm-6">
+                            <div class="row">
+                                <div class="col-sm">
+                                    <div class="form-group">
+                                        <label for="time"><i class="far fa-clock"></i> 時間</label>
+                                        <input type="text" class="form-control disable" id="time" placeholder="系統自動填入，請勿更動" disabled>
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="location"><i class="fas fa-location-arrow"></i> 地點</label>
-                                    <input type="text" class="form-control disable" id="location" placeholder="系統自動填入，請勿更動" disabled>
+                                <div class="col-sm">
+                                    <div class="form-group">
+                                        <label for="location"><i class="fas fa-location-arrow"></i> 地點</label>
+                                        <input type="text" class="form-control disable" id="location" placeholder="系統自動填入，請勿更動" disabled>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-sm">
-                                <div class="form-group">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group stu-in">
+                                        <label for="identify"><i class="fas fa-camera"></i> Scaner</label>
+                                        <div class="camera">
+                                            @include('covid.frontend.api.instacam')
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {{--
+                            <div class="form-group">
+                                <label for="note"><i class="far fa-sticky-note"></i> 備註</label>
+                                <textarea class="form-control is-invalid" id="note" placeholder="備註" ></textarea>
+                            </div>
+                            
+                            <button type="submit" class="btn btn-lg btn-primary">送出填報 <i class="fab fa-telegram-plane"></i></button>
+                            --}}
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="container stu-in">
+                            <div class="row">
+                                <div class="form-group stu-in">
                                     <label for="identify"><i class="fas fa-qrcode"></i> QR ID</label>
-                                    <input type="text" class="form-control" id="identify" placeholder="QR ID" required autofocus autocomplete="off" onMouseOver="this.select()">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" id="identify" placeholder="QR ID" required autofocus autocomplete="off" onMouseOver="this.select()">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="submit">送出填報 <i class="fab fa-telegram-plane"></i></button>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="identify"><i class="fas fa-camera"></i> Scaner</label>
-                                    <div>{ camera }<br>{ camera }<br>{ camera }<br>{ camera }<br></div>
+                                <div class="stu-in">
+                                    <label><i class="fas fa-tasks"></i> 近期掃入清單</label>
+                                    <button type="button" class="badge btn-sm btn-outline-danger">清除</button>
                                 </div>
-                            </div>
-                        </div>
-                        {{--
-                        <div class="form-group">
-                            <label for="note"><i class="far fa-sticky-note"></i> 備註</label>
-                            <textarea class="form-control is-invalid" id="note" placeholder="備註" ></textarea>
-                        </div>
-                        --}}
-                        <button type="submit" class="btn btn-lg btn-primary">送出填報 <i class="fab fa-telegram-plane"></i></button>
-                    </form>
-                </div>
-                <div class="col-sm-6">
-                    <div class="container stu-in">
-                        <div class="row">
-                            <div class="stu-in">
-                                <label><i class="fas fa-tasks"></i> 近期掃入清單</label>
-                                <button type="button" class="badge btn-sm btn-outline-danger">清除</button>
-                            </div>
-                            <div class="alert alert-success stu-in">
-                                <i class="fas fa-id-card-alt"></i> 陳芳同 <span class="text-dark">已在 20201105 19:32:59 刷入</span>
-                            </div>
-                            <div class="alert alert-danger stu-in">
-                                <i class="fas fa-id-card-alt"></i> 高勇成 <span class="text-dark">已在 20201105 19:30:31 刷入</span>
+                                <div class="alert alert-success stu-in">
+                                    <i class="fas fa-id-card-alt"></i> 陳芳同 <span class="text-dark">已在 20201105 19:32:59 刷入</span>
+                                </div>
+                                <div class="alert alert-danger stu-in">
+                                    <i class="fas fa-id-card-alt"></i> 高勇成 <span class="text-dark">已在 20201105 19:30:31 刷入</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     @endsection
 @endsection
