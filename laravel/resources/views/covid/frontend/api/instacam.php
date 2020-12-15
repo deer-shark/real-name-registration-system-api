3600e8db4bae8385d35c5fdd8a36a7c8591123a0
You Cheng Huang <53697217+yc97463@users.noreply.github.com>
2020-12-15 23:53:19 +0800

    update

--- laravel/resources/views/covid/frontend/api/instacam.php
+++ laravel/resources/views/covid/frontend/api/instacam.php
@@ -1,49 +1,7 @@
 <video id="preview" style="width:100%;height:50%;"></video>
 <!-- <textarea id="result" rows="4" cols="50"></textarea> -->
 <script type="text/javascript">
-    let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-    scanner.addListener('scan', function (content) {
-        var output = content
-        // console.log(output);
-        const baseUrl = 'https://rnrs2.imych.one/api';
-        const guest_hash = output;
-        var res = request('GET',`/register/hash/${guest_hash}`);
-        if(res.code==200){
-            Toast.fire({
-                icon: 'success',
-                title: '刷入成功 at'+dayjs().format('HH:mm:ss')
-            });
-            //$("#recently").prepend(`<div class="alert alert-danger stu-in"><i class="fas fa-id-card-alt"></i> ${res.data.guest} <span class="text-dark">已在 20201105 19:30:31 刷入</span></div>`);
-            var guest=res.data[0];
-            $("#stu-name").text(guest.student_id+' '+guest.name);
-            $("#stu-class").val(guest.class);
-            $("#stu-seat").val(guest.seat);
-        }else
-            alert('Error!!!');
-        
-
-
-        /*fetch(`${baseUrl}/register/hash/${guest_hash}`, {
-            method: 'GET',
-            cors: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(response => {
-            return response.json()
-        }).then(result => {
-            Toast.fire({
-                icon: 'success',
-                title: '刷入成功'
-            });
-            $("#recently").prepend('<div class="alert alert-danger stu-in"><i class="fas fa-id-card-alt"></i> 測試用 <span class="text-dark">已在 20201105 19:30:31 刷入</span></div>');
-            $("#stu.name").replaceWith(content)
-            // $('#result')[0].value = JSON.stringify(result);
-            
-            // alert(`ID: ${result.id}／檢驗項目：${result.code.coding[0].display}／檢驗值：${result.valueQuantity.value} ${result.valueQuantity.unit}`)
-
-        });*/
-    });
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
     Instascan.Camera.getCameras().then(function (cameras){
                     if(cameras.length>0){
                         scanner.start(cameras[0]);
