39a8460320c0ec54fe718a2f3047a66bb2aa315a
Sz-Wei Chen <swchen1217@gmail.com>
2020-12-22 16:47:11 +0800

    fix h1 rwd

--- laravel/resources/views/covid/frontend/layouts/header.blade.php
+++ laravel/resources/views/covid/frontend/layouts/header.blade.php
@@ -1,7 +1,15 @@
 <section class="header">
     <header class="jumbotron banner">
         <div class="text-center" id="title">
-            <h1>雄中雄女實名制進場整合系統</h1>
+	    <h1>雄中雄女實名制進場整合系統</h1>
+<style type="text/css">
+@media (max-width: 670px) {
+h1{
+font-size:2rem;
+}
+}
+</style>
+
         </div>
         @include('covid.frontend.layouts.navbar-kshs')
     </header>
